3cf9817afcf687b8c11ea874c40d191afb740e36
MrUser <user1@localhost.localdomain>
2018-02-12 11:11:35 -0600

    working

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Users from "../components/Users";
+
+
+function mapStateToProps(state){
+  return {
+
+    users: state.users,
+
+    sortOn:state.currentUserSort,
+
+    firstNameFilter: state.searchText
+}
+}
+
+export default connect(mapStateToProps)(Users);
